a856a191001ce89633ffb9c39ccd3c09bd952e98
Diego Napoli <diego.napoli@buser.com.br>
2022-04-14 13:21:33 -0300

    Add file upload test

--- django_connexion/tests/testapp/apis.py
+++ django_connexion/tests/testapp/apis.py
@@ -1,3 +1,11 @@
+import os
 from django_connexion import DjangoApi
 
-helloworld_api = DjangoApi('django_connexion/tests/testapp/openapi/helloworld-api.yaml')
+def helper(file_name):
+    dirname = os.path.dirname(__file__)
+    filename = os.path.abspath(os.path.join(dirname, f'../testapp/openapi/{file_name}.yaml'))
+    return filename
+
+
+helloworld_api = DjangoApi(helper('helloworld-api'))
+upload_api = DjangoApi(helper('file-upload'))
--- django_connexion/tests/testapp/urls.py
+++ django_connexion/tests/testapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from django_connexion.tests.testapp.apis import helloworld_api
+from django_connexion.tests.testapp.apis import helloworld_api, upload_api
 
 urlpatterns = [
     path('helloworld/', helloworld_api.urls),
+    path('file/', upload_api.urls),
 ]
